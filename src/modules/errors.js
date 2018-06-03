3bac32b32be6bcfb76aa419f3a67ab4762128ab0
lhazuca <lhazuca@gmail.com>
2018-06-03 01:40:42 -0300

    archivo faltante de modulo de errores

---
+++ src/modules/errors.js
@@ -0,0 +1,31 @@
+class APIError extends Error {
+  constructor(name,statusCode,errorCode,message=null){
+    super(message || name);
+    this.name = name;
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+  }
+}
+
+class ResourceNotFound extends APIError {
+  constructor(){
+    super('ResourceNotFound',409,'RESOURCE_NOT_FOUND');
+  }
+}
+function errorHandler(err, req, res, next) {
+  console.error(err); // imprimimos el error en consola
+  // Chequeamos que tipo de error es y actuamos en consecuencia
+  if (err instanceof APIError){
+    res.status(err.statusCode);
+    res.json({status: err.statusCode, errorCode: err.errorCode});
+  } else {
+  // continua con el manejador de errores por defecto
+    next(err);
+  }
+}
+
+module.exports = {
+  APIError,
+  ResourceNotFound,
+  errorHandler 
+};
